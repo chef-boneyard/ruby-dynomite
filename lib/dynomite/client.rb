24f752f3bf9d64a3552846a8a2e9f3742ad97541
Christopher Brown <cb@opscode.com>
2009-03-12 16:17:46 -0700

    now my client inherits from the thrift-generated one

--- lib/dynomite/client.rb
+++ lib/dynomite/client.rb
@@ -6,23 +6,23 @@ require 'thrift/protocol/tbinaryprotocol'
 require 'dynomite/config'
 require 'dynomite/launcher'
 
-$:.unshift(File.join(File.dirname(__FILE__), "..", "gen-rb"))
-require "Dynomite.rb"
+$:.unshift(File.join(File.dirname(__FILE__), "..", "thrift"))
+require 'Dynomite.rb'
 
-class DynomiteClient
+class DynomiteClient < Dynomite::Client
 
   attr_reader :config
   protected :config
   
   def initialize(params={ })
     @config = { :host=>'localhost', :port=>9200, :transport=>Thrift::TBufferedTransport, :protocol=>Thrift::TBinaryProtocol }.merge(params)
+    @socket = Thrift::TSocket.new(config[:host], config[:port])
+    @protocol = config[:protocol].new(config[:transport].new(@socket))
+    super(@protocol)
   end
 
   def connect
-    @socket = Thrift::TSocket.new(config[:host], config[:port])
     @socket.open
-    @protocol = config[:protocol].new(config[:transport].new(@socket))
-    @client = Dynomite::Client.new(protocol)
   end
 
   def disconnect
--- lib/thrift/Dynomite.rb
+++ lib/thrift/Dynomite.rb
@@ -4,8 +4,8 @@
 # DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
 #
 
+require 'thrift'
 require 'thrift/protocol/tprotocol'
-require 'thrift/thrift'
 require 'dynomite_types'
 
 module Dynomite
